57bbf2ea19da77bafabd8095146c4bbb11420612
Meghanath Shetty <meghanathshetty108@gmail.com>
2022-08-10 11:23:57 +0530

    added 3 function
    printStack, isEmpty  and isFull function is added

--- Stack/app/StackArr.java
+++ Stack/app/StackArr.java
@@ -1,5 +1,40 @@
 package Stack.app;
 
 public class StackArr {
+    private int top,max;
+    int arr[];
+    StackArr(int size){
+        arr = new int [size];
+        max = size;
+        top=-1;
+    }
+
+        //isEmpty
+        public boolean isEmpty(){
+            return (top==-1);
+        }
     
+        //isFull
+        public boolean isFull(){
+            return (max==top+1);
+        }
+    
+        //push
+    
+    
+        //pop
+
+    //print stack
+    public void printStack(){
+        if(isEmpty()){
+            System.out.println("Stack is empty");
+            return;
+        }
+
+        for (int i = 0; i <= top; i++) {
+            System.out.println(arr[top]);
+        }
+    }
 }
+
+
