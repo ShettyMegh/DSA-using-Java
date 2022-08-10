bf71e585569fb15d5b40d53140fcdb8e43eba33c
Meghanath Shetty <meghanathshetty108@gmail.com>
2022-08-10 12:52:21 +0530

    Stack using Singly Linked List

---
+++ Stack/app/StackLL.java
@@ -0,0 +1,81 @@
+package app;
+
+public class StackLL {
+    class Node{
+        int data;
+        Node next;
+        Node(int ele){
+            this.data = ele;
+            this.next = null;
+        }
+    }
+
+
+    Node head;
+    int top,max;
+
+    public StackLL(int size){
+        this.max = size;
+        this.top = -1;
+    }
+
+    //isEmpty
+    public boolean isEmpty(){
+        return (top==-1);
+    }
+
+    //isFull
+    public boolean isFull(){
+        return (max==top+1);
+    }
+
+    //push
+    public boolean push(int ele){
+        if(isFull()) return false;
+        Node newNode = new Node(ele);
+        if(head==null){
+            head = newNode;
+            top++;
+            return true;
+        }
+
+        newNode.next = head;
+        head = newNode;
+        top++;
+        return true;
+    }
+
+    //pop
+    public int pop(){
+        if(isEmpty()) return -1;
+
+        int res = head.data;
+        head = head.next;
+        top--;
+        return res;
+
+        
+    }
+
+    //print
+    public void  printStack(){
+        if(isEmpty()){
+            System.out.println("Stack is Empty");
+            return;
+        }
+
+        Node current = head;
+
+        while(current !=null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+        
+    }
+
+
+
+
+
+
+}
