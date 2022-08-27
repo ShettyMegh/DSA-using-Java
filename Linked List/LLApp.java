7aab11c2ac67b4dba52d72f6a021b9db94f72ed5
Meghanath Shetty <meghanathshetty108@gmail.com>
2022-08-27 22:26:31 +0530

    main app added skeleton

--- Linked List/LLApp.java
+++ Linked List/LLApp.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
+
 import packages.SLL;
 
 class LLApp{
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+
+        
+        System.out.println("Welcome to Linked List App");
+
+        boolean flag = true;
+
+        while(flag){
+            System.out.println("1.Singly Linked List \t 2.Circular Singly Linked List \n 3.Doubly Linked List \t 4.Double Circular Linked List \n 5.Exit");
+            System.out.println("Enter you choice: ");
+            int choice = sc.nextInt();
+            switch(choice){
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+                    
+                default:
+                    break;
+            }
+        }
 
 
     }
