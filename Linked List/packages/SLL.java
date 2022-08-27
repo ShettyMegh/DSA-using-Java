d5e34b270b3effdd41f149e63ebdee7d3ce046ca
Meghanath Shetty <meghanathshetty108@gmail.com>
2022-08-27 18:05:08 +0530

    inner node class

---
+++ Linked List/packages/SLL.java
@@ -0,0 +1,20 @@
+package packages;
+
+public class SLL {
+
+
+    //Node creation
+    private class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+
+
+}
