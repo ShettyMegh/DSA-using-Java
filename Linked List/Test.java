1f54bf830841e35c6a4c9c5e732698968da5ec23
Meghanath Shetty <meghanathshetty108@gmail.com>
2022-08-28 10:43:54 +0530

    add from back
    print list not working properly

---
+++ Linked List/Test.java
@@ -0,0 +1,13 @@
+import packages.*;
+
+public class Test {
+    public static void main(String[] args) {
+        CSLL csll = new CSLL();
+       
+        csll.printList();
+        csll.addBack(33);
+        csll.addBack(11);
+        csll.printList();
+
+    }
+}
--- Linked List/packages/CSLL.java
+++ Linked List/packages/CSLL.java
@@ -9,8 +9,29 @@ public class CSLL {
             this.next = null;
         }
 
+    }
+
         Node head,tail;
 
+
+
+        //print list
+        public void printList(){
+            if(head==null){
+                System.out.println("List is empty");
+                return;
+            }
+
+            Node current = head;
+
+            while(current.next != head){
+                System.out.print(current.data+" -> ");
+                current = current.next;
+            }
+            System.out.println(tail.next.data+"(head)");
+
+
+        }
 
         //add from front
         public void addFront(int data){
@@ -25,5 +46,20 @@ public class CSLL {
             tail.next = head;
         }
 
+
+
+          //add from back
+          public void addBack(int data){
+            Node newNode = new Node(data);
+            if(head==null){
+                head = tail = newNode;
+                return;
+            }
+
+           tail.next = newNode;
+           tail = newNode;
+           tail.next = head;
+        }
+
+
     }
-}
